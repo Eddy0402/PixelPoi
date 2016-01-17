d8ba2e29eec44b8bb4ac4fe3d6507389c6693615
Eddy Wu <yukkcake@gmail.com>
2016-01-17 23:37:16 +0800

    Add a faster clock configuration

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,69 @@
 #include "driverlib.h"
 #include "led.hpp"
 
+#define UCS_MCLK_DESIRED_FREQUENCY_IN_KHZ   12000
+#define UCS_MCLK_FLLREF_RATIO   366
+
+/*
+ * Init clock system as follow:
+ * MCLK:
+ * SMCLK:
+ * ACLK:
+ */
+void initUCS(){
+    //Set VCore = 1 for 12MHz clock
+    PMM_setVCore(PMM_CORE_LEVEL_1);
+
+    //Set DCO FLL reference = REFO
+    UCS_initClockSignal(
+        UCS_FLLREF,
+        UCS_REFOCLK_SELECT,
+        UCS_CLOCK_DIVIDER_1
+        );
+    //Set ACLK = REFO
+    UCS_initClockSignal(
+        UCS_ACLK,
+        UCS_REFOCLK_SELECT,
+        UCS_CLOCK_DIVIDER_1
+        );
+
+    //Set Ratio and Desired MCLK Frequency  and initialize DCO
+    UCS_initFLLSettle(
+        UCS_MCLK_DESIRED_FREQUENCY_IN_KHZ,
+        UCS_MCLK_FLLREF_RATIO
+        );
+
+    // Enable global oscillator fault flag
+    SFR_clearInterrupt(SFR_OSCILLATOR_FAULT_INTERRUPT);
+    SFR_enableInterrupt(SFR_OSCILLATOR_FAULT_INTERRUPT);
+
+    // Enable global interrupt
+    __bis_SR_register(GIE);
+}
+
+uint16_t status;
+__attribute__((interrupt(UNMI_VECTOR)))
+void NMI_ISR(void)
+{
+    do
+    {
+        // If it still can't clear the oscillator fault flags after the timeout,
+        // trap and wait here.
+        status = UCS_clearAllOscFlagsWithTimeout(1000);
+    }
+    while(status != 0);
+}
+
 int main(void)
 {
     // Stop WDT_A
     WDT_A_hold(WDT_A_BASE);
 
-    LedController::init();
-    LedController::appSetup();
-    LedController::start();
+    initUCS();
+
+//    LedController::init();
+//    LedController::appSetup();
+//    LedController::start();
 
     __bis_SR_register(LPM0_bits + GIE);
 
